cedb8f9546ddbda614d051d1f87c38387caab1de
Charline SEBELLIN <20190177@B15-06.local>
2019-11-07 16:01:30 +0100

    Modif Exercice 005

--- episode.005/src/spaceAge.js
+++ episode.005/src/spaceAge.js
@@ -1,13 +1,27 @@
 export default function spaceAge(planet,number) {  
-    let time = ''
-    if(planet = "Earth"){
-        const earth = number/31557600
+    var time = ''
+    if(planet == "Earth"){
+        let earth = number/31557600
         time = earth 
     }
-    else if(planet = "Mercury"){
-        time = earth*0.2408467
+    if(planet == "Mercury"){
+        time = earth/0.2408467
     }
-    
+    if(planet == "Venus"){
+        time = earth/0.61519726
+    }   
+    if(planet == "Jupiter"){
+        time = earth/1.8808158
+    }   
+    if(planet == "Saturn"){
+        time = earth/11.862615
+    }   
+    if(planet == "Uranus"){
+        time = earth/29.447498
+    }
+    if(planet == "Neptune"){
+        time = earth/0.2408467
+    }         
     return parseFloat(time.toFixed(2))
 
   }
