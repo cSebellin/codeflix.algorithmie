74c9e1da83c9a68e41eed01ce8c119b42e2a76e1
Charline SEBELLIN <20190177@B15-06.local>
2019-11-08 08:40:25 +0100

    Episode 5 terminé

--- episode.005/src/spaceAge.js
+++ episode.005/src/spaceAge.js
@@ -1,27 +1,31 @@
 export default function spaceAge(planet,number) {  
     var time = ''
-    if(planet == "Earth"){
-        let earth = number/31557600
-        time = earth 
-    }
-    if(planet == "Mercury"){
-        time = earth/0.2408467
-    }
-    if(planet == "Venus"){
-        time = earth/0.61519726
-    }   
-    if(planet == "Jupiter"){
-        time = earth/1.8808158
-    }   
-    if(planet == "Saturn"){
-        time = earth/11.862615
-    }   
-    if(planet == "Uranus"){
-        time = earth/29.447498
-    }
-    if(planet == "Neptune"){
-        time = earth/0.2408467
-    }         
+    let earth = number/31557600
+        if(planet == "Earth"){
+            time = earth 
+        }
+        if(planet == "Mercury"){
+            time = earth/0.2408467
+        }
+        if(planet == "Venus"){
+            time = earth/0.61519726
+        }
+        if(planet == "Mars"){
+            time = earth/1.8808158
+        }      
+        if(planet == "Jupiter"){
+            time = earth/11.862615
+        }   
+        if(planet == "Saturn"){
+            time = earth/29.447498
+        }   
+        if(planet == "Uranus"){
+            time = earth/84.016846
+        }
+        if(planet == "Neptune"){
+            time = earth/164.79132
+        }
+             
     return parseFloat(time.toFixed(2))
 
   }
